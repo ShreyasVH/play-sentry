11d25d5c802ff4b2ad3668753fad3be8cc4b090c
Shreyas Hande <shreyas@everwell.org>
2022-11-06 22:37:38 +0530

    initial commit

---
+++ app/controllers/IndexController.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import play.Logger;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.Http;
+
+public class IndexController extends Controller
+{
+	private final Logger.ALogger logger = Logger.of(IndexController.class);
+
+	public Result get(String input)
+	{
+		logger.trace("Trace logging");
+		logger.debug("Debug logging");
+		logger.info("Info logging");
+		logger.warn("Warn logging");
+		logger.error("Error logging");
+
+		return ok("GET REQUEST with input: " + input);
+	}
+
+	public Result post(Http.Request request)
+	{
+		return ok("POST REQUEST with payload: " + request.body().asJson().toString());
+	}
+
+	public Result put(Http.Request request)
+	{
+		return ok("PUT REQUEST with payload: " + request.body().asJson().toString());
+	}
+
+	public Result delete()
+	{
+		return ok("DELETE REQUEST");
+	}
+}
---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "play-sentry"
+
+version := "1.0.0"
+
+scalaVersion := "2.13.6"
+
+libraryDependencies += guice
+libraryDependencies += "io.sentry" % "sentry-logback" % "1.7.30"
+
+lazy val root = (project in file(".")).enablePlugins(PlayJava)
